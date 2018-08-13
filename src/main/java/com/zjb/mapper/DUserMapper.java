5057014a5630c3566a16b57232da36eac192e41a
Administrator <fillixzhang@163.com>
2018-08-14 00:45:18 +0800

    配置mybatis自动生成model、mapper、mapper.xml

---
+++ src/main/java/com/zjb/mapper/DUserMapper.java
@@ -0,0 +1,7 @@
+package com.zjb.mapper;
+
+import com.zjb.model.DUser;
+import com.zjb.utils.MyMapper;
+
+public interface DUserMapper extends MyMapper<DUser> {
+}
---
+++ src/main/java/com/zjb/model/DUser.java
@@ -0,0 +1,69 @@
+package com.zjb.model;
+
+import javax.persistence.*;
+
+@Table(name = "d_user")
+public class DUser {
+    @Id
+    private String id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * @return id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取用户名
+     *
+     * @return username - 用户名
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * 设置用户名
+     *
+     * @param username 用户名
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * 获取密码
+     *
+     * @return password - 密码
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * 设置密码
+     *
+     * @param password 密码
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
